7294c3d8af109f96f848c351c0f4df04f3534fa7
neroexpress <nkneroexpress@gmail.com>
2018-03-21 23:08:12 -0500

    First commit

---
+++ MVCSimpleWebgrid/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MVCSimpleWebgrid.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public ActionResult List(string search)
+        {
+            var students = new List<Student>();
+            //here  MyDatabaseEntities is the dbcontext
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                if (search != null)
+                {
+                    students = dc.Students.Where(x => x.StdFirstName.Contains(search)).ToList();
+                }
+                else
+                {
+                    students = dc.Students.ToList();
+                }
+            }
+            return View(students);
+        }
+
+    }
+}
---
+++ MVCSimpleWebgrid/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCSimpleWebgrid.MvcApplication" Language="C#" %>
